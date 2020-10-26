31ba320dfd0f2ecc39491e65e71b0405f5570f33
xiaohaiAI <vip1769120379@163.com>
2020-10-26 19:47:00 +0800

    合并

--- pocomsg/src/App.js
+++ pocomsg/src/App.js
@@ -69,7 +69,7 @@ let addMenu=({key})=>{
           </Content>
         </Layout>
       </Layout>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UEDE</Footer>
     </div>
   );
 }
