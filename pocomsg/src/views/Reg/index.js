24c02e3bc66185581197f60543c03529c7bfef8b
xiaohaiAI <vip1769120379@163.com>
2020-10-27 09:04:34 +0800

    登录、注册静态

--- pocomsg/src/views/Reg/index.js
+++ pocomsg/src/views/Reg/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-function Reg(){
-    return(
-        <div>Reg</div>
-    )
+function Reg(){
+    return(
+        <div>
+            <h1>注册</h1>
+        <Form
+            name="basic"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+                style={{width:600,marginBottom:20,marginLeft:250}}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+                style={{width:600,marginBottom:20,marginLeft:250}}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked">
+                {/* <Checkbox>记住我？</Checkbox> */}
+            </Form.Item>
+
+            <Form.Item >
+                <Button type="primary" htmlType="submit">
+                    注册
+            </Button>
+            </Form.Item>
+        </Form>
+    </div>
+    )
 }
 
-export default Reg
+export default Reg
