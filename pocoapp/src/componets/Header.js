ef62c02168f815016a645974fb26f7364f3edb39
lxfan1998 <lxfan1998@163.com>
2020-10-28 15:48:46 +0800

    导航、首页

--- pocoapp/src/componets/Header.js
+++ pocoapp/src/componets/Header.js
@@ -3,7 +3,7 @@ import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 import '../assets/css/Header.css';
 class App1 extends React.Component {
   state = {
-    open: true,
+    open: false,
   }
   onOpenChange = (...args) => {
     console.log(args);
@@ -11,20 +11,25 @@ class App1 extends React.Component {
   }
   render() {
     // fix in codepen
-    const sidebar = <List>
-      <List.Item>
-        <div>
-          登录/注册
+    const sidebar =( <List>
+      <div className="texttop">
+        <span className="user"></span>
+        <span className="usertext">登录/注册</span>
+      </div>
+      <ul>
+        <li>首页</li>
+        <li>作品</li>
+        <li>图片广场</li>
+        <li>摄影技巧</li>
+        <li>对话</li>
+        <li>极限摄影</li>
+        <li>POCO认证</li>
+      </ul>
+      <div className="listfooter">
+        <span className="listfooterImg"></span>
+        <span>></span>
         </div>
-      </List.Item>
-      <List.Item>首页</List.Item>
-      <List.Item>作品</List.Item>
-      <List.Item>图片广场</List.Item>
-      <List.Item>摄影技巧</List.Item>
-      <List.Item>对话</List.Item>
-      <List.Item>极限摄影</List.Item>
-      <List.Item>POCO认证</List.Item>
-    </List>;
+    </List>);
 
     return (<div>
       <NavBar style={{backgroundColor:"#000",fontSize:".18rem"}} icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>POCO摄影</NavBar>
@@ -32,11 +37,10 @@ class App1 extends React.Component {
         className="my-drawer"
         style={{ minHeight: document.documentElement.clientHeight }}
         enableDragHandle
-        contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: .42 }}
+        contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
         sidebar={sidebar}
         open={this.state.open}
         onOpenChange={this.onOpenChange}
-        dragToggleDistance='.8rem'
       >
        rrr
       </Drawer>
--- pocoapp/src/views/Home/index.jsx
+++ pocoapp/src/views/Home/index.jsx
@@ -1,22 +1,62 @@
 import React from 'react';
 import Header from '../../componets/Header';
+import { Carousel, WingBlank } from 'antd-mobile';
 
-
-function Home(){
-    return(
+class Home extends React.Component {
+  state = {
+    data: ['1', '2', '3'],
+    imgHeight: 176,
+  }
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+      });
+    }, 100);
+  }
+  render() {
+    return (
         <div>
             <div>
-           <div className="header">
-                <Header></Header>
-           </div>
-            <div className="content">
-                <div className="content-con">
-                        1
+                <div className="header">
+                    <Header></Header>
+                </div>
+                <div className="content">
+                    <div className="content-con">
+                        <WingBlank>
+                            <Carousel
+                            autoplay={false}
+                            infinite
+                            beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                            afterChange={index => console.log('slide to', index)}
+                            >
+                            {this.state.data.map(val => (
+                                <a
+                                key={val}
+                                href="http://www.alipay.com"
+                                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                                >
+                                <img
+                                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                    alt=""
+                                    style={{ width: '100%', verticalAlign: 'top' }}
+                                    onLoad={() => {
+                                    // fire window resize event to change height
+                                    window.dispatchEvent(new Event('resize'));
+                                    this.setState({ imgHeight: 'auto' });
+                                    }}
+                                />
+                                </a>
+                            ))}
+                            </Carousel>
+                        </WingBlank>
+                    </div>
                 </div>
             </div>
-            </div>
         </div>
-    )
+    );
+  }
 }
 
-export default Home
+export default Home;
