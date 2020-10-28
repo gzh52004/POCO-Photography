ddbcde403b01442bd5947c1c44249f50a41367c5
lxfan1998 <lxfan1998@163.com>
2020-10-28 10:31:09 +0800

    List.Item

--- pocoapp/src/componets/Header.js
+++ pocoapp/src/componets/Header.js
@@ -3,7 +3,7 @@ import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 import '../assets/css/Header.css';
 class App1 extends React.Component {
   state = {
-    open: false,
+    open: true,
   }
   onOpenChange = (...args) => {
     console.log(args);
@@ -12,34 +12,31 @@ class App1 extends React.Component {
   render() {
     // fix in codepen
     const sidebar = <List>
-     <List.Item>
+      <List.Item>
         <div>
-          登录、注册
+          登录/注册
         </div>
-      
-          <ul>
-            <li>首页</li>
-            <li>作品</li>
-            <li>图片广场</li>
-            <li>摄影技巧</li>
-            <li>对话</li>
-            <li>极限摄影</li>
-            <li>POCO认证</li>
-          </ul>
-        
-     </List.Item>
-    </List>
+      </List.Item>
+      <List.Item>首页</List.Item>
+      <List.Item>作品</List.Item>
+      <List.Item>图片广场</List.Item>
+      <List.Item>摄影技巧</List.Item>
+      <List.Item>对话</List.Item>
+      <List.Item>极限摄影</List.Item>
+      <List.Item>POCO认证</List.Item>
+    </List>;
 
     return (<div>
-      <NavBar style={{backgroundColor:"#000",fontSize:".14rem"}} icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>POCO摄影</NavBar>
+      <NavBar style={{backgroundColor:"#000",fontSize:".18rem"}} icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>POCO摄影</NavBar>
       <Drawer
         className="my-drawer"
         style={{ minHeight: document.documentElement.clientHeight }}
         enableDragHandle
-        contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+        contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: .42 }}
         sidebar={sidebar}
         open={this.state.open}
         onOpenChange={this.onOpenChange}
+        dragToggleDistance='.8rem'
       >
        rrr
       </Drawer>
