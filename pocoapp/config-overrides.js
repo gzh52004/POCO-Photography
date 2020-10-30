230399270dc7c98f4c53ac49bfa75313c96cad2b
liweijun <1351965332@qq.com>
2020-10-30 12:00:01 +0800

    修改config

--- pocoapp/config-overrides.js
+++ pocoapp/config-overrides.js
@@ -1,34 +1,42 @@
-const path = require('path');
-console.log(require)
-// react-app-rewired@1.x的写法
-// const { injectBabelPlugin } = require('react-app-rewired');
-// module.exports = function override(config, env) {
-//     // 修改配置
-//     config.resolve.alias['@'] = path.join(__dirname,'./src/')
-//      config = injectBabelPlugin([
-//          "@babel/plugin-proposal-decorators", { "legacy": true }
-//      ], config);
+const path = require('path');
+const { injectBabelPlugin } = require('react-app-rewired');
+const {override,addDecoratorsLegacy,addWebpackAlias,fixBabelImports,useBabelRc,disableEsLint} = require('customize-cra')
+// module.exports =  function(config, env) {
+//     // config: webpack的配置
+//     console.log('env',env);
+//     console.log(config)
 
-//      return config;
-//  }
+//     // 修改webpack配置
+//     config.resolve.alias['@'] = path.resolve(__dirname,'src')
+//     // config.module.rules[0].use[0].options.plugins.unshift(["@babel/plugin-proposal-decorators", { "legacy": true }])
 
-// react-app-rewired@2.x的写法
-const {override,addDecoratorsLegacy,disableEsLint,useBabelRc,fixBabelImports,addWebpackAlias} = require('customize-cra');
+//     // 
+//     config = injectBabelPlugin([
+//         "@babel/plugin-proposal-decorators", { "legacy": true }
+//     ], config);
 
-module.exports = override(
-    // 支持ES7装饰器
-    addDecoratorsLegacy(),
+//     return config
+// }
 
-    // 禁用ESlinit
-    disableEsLint(),
+module.exports = override(
+    addDecoratorsLegacy(), // 支持装饰器
+    addWebpackAlias({
+        '@':path.join(__dirname,'src'), // D:\app\xx, /app/xxx
+        '#': path.join(__dirname,'src/components'),
+        '~': path.join(__dirname,'src/views')
+    }), // 添加路径别名
 
-    // 添加别名
-    addWebpackAlias({
-        '@':path.join(__dirname,'./src/'),
-        '~':path.join(__dirname,'./src/components')
-    }),
+    // ui框架按需加载
+    fixBabelImports('import',{
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": "css" // `style: true` 会加载 less 文件
+    }),
 
-    // 按需加载antd
-    fixBabelImports('import',{ libraryName: "antd", style: "css" },'antd'),
-    // fixBabelImports('import',{ libraryName: "antd-mobile", style: "css" },'antdm')
+    // 使用.babelrc配置
+    // useBabelRc(),
+
+    // 禁用eslint
+    disableEsLint()
+      
 )
--- pocomsg/config-overrides.js
+++ pocomsg/config-overrides.js
@@ -1,17 +1,42 @@
-const path=require('path');
-const {override,addDecoratorsLegacy,disableEsLint,useBabelRc,fixBabelImports}=require('customize-cra');
-module.exports=override(
-    // 支持ES7装饰器
-    addDecoratorsLegacy(),
+const path = require('path');
+const { injectBabelPlugin } = require('react-app-rewired');
+const {override,addDecoratorsLegacy,addWebpackAlias,fixBabelImports,useBabelRc,disableEsLint} = require('customize-cra')
+// module.exports =  function(config, env) {
+//     // config: webpack的配置
+//     console.log('env',env);
+//     console.log(config)
 
-    // 禁用ESlint
-    disableEsLint(),
+//     // 修改webpack配置
+//     config.resolve.alias['@'] = path.resolve(__dirname,'src')
+//     // config.module.rules[0].use[0].options.plugins.unshift(["@babel/plugin-proposal-decorators", { "legacy": true }])
 
-    // 添加别名
-    useBabelRc({
-        '@':path.join(__dirname,'./src/'),
+//     // 
+//     config = injectBabelPlugin([
+//         "@babel/plugin-proposal-decorators", { "legacy": true }
+//     ], config);
+
+//     return config
+// }
+
+module.exports = override(
+    addDecoratorsLegacy(), // 支持装饰器
+    addWebpackAlias({
+        '@':path.join(__dirname,'src'), // D:\app\xx, /app/xxx
+        '#': path.join(__dirname,'src/components'),
+        '~': path.join(__dirname,'src/views')
+    }), // 添加路径别名
+
+    // ui框架按需加载
+    fixBabelImports('import',{
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": "css" // `style: true` 会加载 less 文件
     }),
 
-    // 按需加载antd
-    fixBabelImports('import',{libraryName:"antd",style:"css"},"antd")
+    // 使用.babelrc配置
+    // useBabelRc(),
+
+    // 禁用eslint
+    disableEsLint()
+      
 )
