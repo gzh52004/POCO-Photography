f76362814ede59dc162f445a3624655101941d8e
lxfan1998 <lxfan1998@163.com>
2020-10-31 15:04:03 +0800

    给导航列表添加样式

--- pocoapp/src/componets/Home/Header.js
+++ pocoapp/src/componets/Home/Header.js
@@ -1,15 +1,58 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 import '../../assets/css/Home/Header.css';
+import Home from '../../views/Home';
+import Mine from '../../views/Mine/index.jsx';
+import Talks from '../../views/Talks';
+import Works from '../../views/Works';
+import LimitPhotography from '../../views/LimitPhotography';
+import Identification from '../../views/Identification';
+import PhotographyTechnique from '../../views/PhotographyTechnique';
+import ImageSquare from '../../views/ImageSquare';
 class App1 extends React.Component {
   state = {
     open: false,
+    menu:[
+      {
+        text:'我的',
+        path:'/mine',
+        name:'mine',
+        component:Mine,
+        // icon:<UserOutlined/>
+      },
+      {
+        text:'首页',
+        path:'/home',
+        name:'home',
+        component:Home,
+        // icon:<HomeOutlined/>
+      },
+      {
+        text:'作品',
+        path:'/works',
+        name:'works',
+        component:Works,
+        // icon:<ShoppingCartOutlined/>
+      },
+      {
+        text:'图片广场',
+        path:'/imagesquare',
+        name:'imagesquare',
+        component:ImageSquare,
+        // icon:<ShoppingCartOutlined/>
+      }
+    ],
+
+    current:'/home',
   }
   onOpenChange = (...args) => {
     console.log(args);
     this.setState({ open: !this.state.open });
   }
+
   render() {
+    // console.log('this.props=',this.props);
     // fix in codepen
     const sidebar =( <List>
       <div className="texttop">
@@ -17,9 +60,14 @@ class App1 extends React.Component {
         <span className="usertext">登录/注册</span>
       </div>
       <ul>
-        <li>首页</li>
+        <li>
+          首页
+        </li>
         <li>作品</li>
-        <li>图片广场</li>
+        <li>
+          <Link to="/imagesquare/" />
+          图片广场
+        </li>
         <li>摄影技巧</li>
         <li>对话</li>
         <li>极限摄影</li>
