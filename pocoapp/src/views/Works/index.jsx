0485c90f9a81bd4d2fe41f73cb9365ea11aac062
lxfan1998 <lxfan1998@163.com>
2020-10-31 15:28:46 +0800

    作品

--- pocoapp/src/views/Works/index.jsx
+++ pocoapp/src/views/Works/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+// import React from 'react';
+import { Icon} from 'antd-mobile';
+import TabExampla from  '../../componets/Works.js'
 
 function Works(){
+    // 对话 Works
     return(
-        <div>Works</div>
+        <div>
+            <div className="header">
+                <div>{<Icon type="ellipsis" />}</div>
+                <div>作品</div>
+                <div>分类</div>
+            </div>
+            <div className="content">
+                <div className="content-con">
+                  {/* 内容 */}
+                   <TabExampla />
+                </div>
+            </div>
+        </div>
     )
 }
 
