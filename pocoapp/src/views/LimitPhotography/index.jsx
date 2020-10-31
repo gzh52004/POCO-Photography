e1b6227eb3487fedb4459bdc2bd5c6994e422abb
lxfan1998 <lxfan1998@163.com>
2020-10-31 15:12:28 +0800

    极限摄影

--- pocoapp/src/views/LimitPhotography/index.jsx
+++ pocoapp/src/views/LimitPhotography/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Icon,SearchBar} from 'antd-mobile';
+import TabExampl from  '../../componets/LimitPhotography.js'
 
 function LimitPhotography(){
+    // 极限摄影
     return(
-        <div>LimitPhotography</div>
+        <div>
+            <div className="header">
+                <div>{<Icon type="ellipsis" />}</div>
+                <div>极限摄影</div>
+                <div></div>
+            </div>
+            <div className="content">
+                <div className="content-con">
+                  {/* 内容 */}
+                  <SearchBar defaultValue="请输入搜索"/>
+                   <TabExampl />
+                </div>
+            </div>
+        </div>
     )
 }
 
