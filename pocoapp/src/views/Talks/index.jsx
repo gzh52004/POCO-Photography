b097ae9fcff560996c4eb6d31da8c2b1088615df
lxfan1998 <lxfan1998@163.com>
2020-10-31 15:27:40 +0800

    会话

--- pocoapp/src/views/Talks/index.jsx
+++ pocoapp/src/views/Talks/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Icon,SearchBar} from 'antd-mobile';
+import TabExamplf from  '../../componets/Talks.js'
 
-function Tallks(){
+function Talks(){
+    // 对话 Talks
     return(
-        <div>Tallks</div>
+        <div>
+            <div className="header">
+                <div>{<Icon type="ellipsis" />}</div>
+                <div>对话</div>
+                <div></div>
+            </div>
+            <div className="content">
+                <div className="content-con">
+                  {/* 内容 */}
+                  <SearchBar defaultValue="请输入搜索"/>
+                   <TabExamplf />
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Tallks
+export default Talks
