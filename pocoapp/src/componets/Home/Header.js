fd78beac71a2ea550e1857a187b08511ffe59d63
lxfan1998 <lxfan1998@163.com>
2020-11-01 22:39:22 +0800

    修改导航样式

--- pocoapp/src/componets/Home/Header.js
+++ pocoapp/src/componets/Home/Header.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 import '../../assets/css/Home/Header.css';
+import home from '../../assets/data/HomeData/list/home.png';
+import works from '../../assets/data/HomeData/list/works.png';
+import photosquare from '../../assets/data/HomeData/list/photosquare.png';
+import skills from '../../assets/data/HomeData/list/skills.png';
+import talks from '../../assets/data/HomeData/list/talks.png';
+import limit from '../../assets/data/HomeData/list/limit.png';
+import verified from '../../assets/data/HomeData/list/verified.png';
+import download from '../../assets/data/HomeData/list/download.png';
+import go from '../../assets/data/HomeData/list/go.png';
+
 class App1 extends React.Component {
   state = {
     open: false,
@@ -19,17 +29,54 @@ class App1 extends React.Component {
         <span className="usertext">登录/注册</span>
       </div>
       <ul>
-        <li><a href='/'>首页</a></li>
-        <li><a href='#/works'>作品</a></li>
-        <li><a href='#/imagesquare'>图片广场</a></li>
-        <li><a href='#/photographytechnique'>摄影技巧</a></li>
-        <li><a href='#/talks'>对话</a></li>
-        <li><a href='#/limitphotography'>极限摄影</a></li>
-        <li><a href='#/identification'>POCO认证</a></li>
+        <li>
+          <a href='/'>
+            <img src={home} />
+            首页
+          </a>
+        </li>
+        <li>
+          <a href='#/works'>
+            <img src={works} />
+            作品
+          </a>
+        </li>
+        <li>
+          <a href='#/imagesquare'>
+            <img src={photosquare} />
+            图片广场
+          </a>
+        </li>
+        <li>
+          <a href='#/photographytechnique'>
+            <img src={skills} />
+            摄影技巧
+          </a>
+        </li>
+        <li>
+          <a href='#/talks'>
+            <img src={talks} />
+            对话
+          </a>
+        </li>
+        <li>
+          <a href='#/limitphotography'>
+            <img src={limit} />
+            极限摄影
+          </a>
+        </li>
+        <li>
+          <a href='#/identification'>
+            <img src={verified} />
+            POCO认证
+          </a>
+        </li>
       </ul>
       <div className="listfooter">
-        <span className="listfooterImg"></span>
-        <span>&gt;</span>
+        <a href='https://www.poco.cn/app/photo'>
+          <img src={download} />
+          <img className='go-icon' src={go} />
+        </a>
         </div>
     </List>);
 
